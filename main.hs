c75d830a78e2748ef3d2f41d9aa43eabd6c2a651
Daniel Buckmaster <dan.buckmaster@gmail.com>
2014-02-13 19:16:41 +1100

    Refactored to only connect when the button is pressed.

--- main.hs
+++ main.hs
@@ -6,10 +6,13 @@ import System.Hardware.Serialport
 
 import Data.List (foldl')
 import Data.Maybe (isJust, fromJust)
-import Text.Regex.Posix ((=~))
 
 import System.Environment (getArgs)
+import Text.Regex.Posix ((=~))
+import Data.IORef (newIORef, writeIORef, readIORef)
+
 import Control.Monad (void, forever, when)
+import Control.Exception (handle, IOException(..))
 import Control.Concurrent (forkIO, killThread)
 import Control.Concurrent.Chan
     (Chan, newChan, dupChan, readChan, writeChan)
@@ -26,51 +29,109 @@ main = do
             tpStatic = Just "static",
             tpLog  = const $ return ()
          }
-        serialConfig = defaultSerialSettings {
-            commSpeed = baud args,
-            bitsPerWord = dataSize args,
-            stopb = stopSize args,
-            parity = paren args
-         }
 
-    incoming <- newChan
-    outgoing <- newChan
-    serial <- openSerial (filePath args) serialConfig
-    reader <- forkIO $ forever $ recvLn serial >>= writeChan incoming
-    writer <- forkIO $ forever $ send   serial =<< readChan  outgoing
+    bus <- newChan
+
+    serialFileRef <- newIORef Nothing
+    settingsRef   <- newIORef args
+
+    serialTask <- forkIO $ serialMonitor bus serialFileRef
 
     putStrLn "Press ENTER to exit."
-    ui <- forkIO $ startGUI guiConfig $ setup incoming outgoing
+    ui <- forkIO $ startGUI guiConfig $ clientSetup bus
     void getLine
 
     killThread ui
-    killThread reader
-    closeSerial serial
+    killThread serialTask
 
--- Sets up the UI for a session. This gets called each time a client connects
--- to the Threepenny server.
-setup :: Bus -> Bus -> Window -> UI ()
-setup incoming outgoing window = void $ do
+type Bus = Chan Message
+
+data Message
+    = Connect Args
+    | Disconnect
+    | Received B.ByteString
+    | Send B.ByteString
+
+serialMonitor mainBus handleRef = do
+    bus <- dupChan mainBus
+    readerRef <- newIORef Nothing
+    writerRef <- newIORef Nothing
+
+    forever $ do
+        e <- readChan bus
+        let noFileHandler :: IOException -> IO ()
+            noFileHandler e = print e >> return ()
+        case e of
+            Connect args -> handle noFileHandler $ do
+                serial <- openSerial (filePath args) defaultSerialSettings {
+                    commSpeed = baud args,
+                    bitsPerWord = dataSize args,
+                    stopb = stopSize args,
+                    parity = paren args
+                 }
+
+                writeIORef handleRef (Just serial)
+
+                reader <- forkIO $ forever $ do
+                    ln <- recvLn serial
+                    writeChan bus $ Received ln
+                writeIORef readerRef (Just reader)
+
+                writer <- forkIO $ forever $ do
+                    ev <- readChan bus
+                    case ev of
+                        Send ln -> send serial ln >> return ()
+                        _ -> return ()
+                writeIORef writerRef (Just writer)
+
+            Disconnect -> do
+                reader <- readIORef readerRef
+                maybeKill reader
+                writer <- readIORef writerRef
+                maybeKill writer
+                serial <- readIORef handleRef
+                maybeClose serial
+                where maybeKill t = case t of
+                        Just t' -> killThread t'
+                        Nothing -> return ()
+                      maybeClose h = case h of
+                        Just h' -> closeSerial h'
+                        Nothing -> return ()
+
+            _ -> return ()
+
+clientSetup :: Bus -> Window -> UI ()
+clientSetup mainBus window = void $ do
+    liftIO $ putStrLn "Accepting new connection"
     return window # set title "Serial"
     addStyleSheets window ["bootstrap.min.css", "console.css"]
 
-    console <- UI.new #. "console"
-    input   <- UI.input
-    navbar  <- mkNavbar [element input]
-    getBody window #+ [element navbar, mkContainer [element console]]
+    bus <- liftIO $ dupChan mainBus
 
-    incoming' <- liftIO $ dupChan incoming
-    reader <- liftIO $ forkIO $ forever $ readChan incoming' >>= addUpdate window console
+    console <- UI.new #. "console"
+    connect <- mkButton "Connect"
+    input   <- UI.input
+    navbar  <- mkNavbar "Serial console" [] [mkNavbarRightForm [element connect]]
+    getBody window #+ [element navbar, mkContainer [element console, element input]]
+
+    reader <- liftIO $ forkIO $ forever $ do
+        e <- readChan bus
+        case e of
+            Received m -> addUpdate window console (B.unpack m) >> return ()
+            _  -> return ()
+
     on UI.disconnect window $ const $ liftIO $ killThread reader
 
-    outgoing' <- liftIO $ dupChan outgoing
+    on UI.click connect $ \_ -> do
+        liftIO $ writeChan bus $ Connect defaultArgs
+
     on UI.sendValue input $ \content -> do
         element input # set value ""
         when (not (null content)) $ liftIO $ do
-            writeChan outgoing' (B.pack content)
+            writeChan bus (Send $ B.pack content)
 
 addUpdate w e u = atomic w $ runUI w $ element e #+
-    [UI.div #. "line" #+ [string $ B.unpack u]]
+    [UI.div #. "line" #+ [string u]]
 
 recvLn :: SerialPort -> IO B.ByteString
 recvLn s = do
@@ -80,14 +141,20 @@ recvLn s = do
         else recvLn s
     return $ first `B.append` rest
 
-mkNavbar contents = UI.div #. "navbar navbar-inverse navbar-fixed-top"
-                           #+ [mkContainer contents]
+mkNavbar title left right = UI.div #.
+    "navbar navbar-inverse navbar-fixed-top" #+
+    [mkContainer $
+        [ UI.div #. "navbar-header" #+ [UI.span # set UI.text title] #. "navbar-brand" ]
+        ++ left ++ right
+    ]
 
 mkNavbarRightForm contents = UI.div #. "navbar-form navbar-right" #+ contents
 
 mkContainer contents = UI.div #. "container" #+ contents
 
 mkButton text = UI.button #. "btn btn-success" # set UI.text text
+
+addStyleSheets w = mapM $ UI.addStyleSheet w
 
 data Args = Args {
     filePath :: FilePath,
@@ -116,32 +183,27 @@ parseArgs = foldl' go defaultArgs
                   sb = case (arg !! 2) of
                     '1' -> One
                     '2' -> Two
-
-type Bus = Chan B.ByteString
-
-addStyleSheets w = mapM $ UI.addStyleSheet w
-
-maybeReadCS s = case s of
-    "CS110" -> Just CS110
-    "CS300" -> Just CS300
-    "CS600" -> Just CS600
-    "CS1200" -> Just CS1200
-    "CS2400" -> Just CS2400
-    "CS4800" -> Just CS4800
-    "CS9600" -> Just CS9600
-    "CS19200" -> Just CS19200
-    "CS38400" -> Just CS38400
-    "CS57600" -> Just CS57600
-    "CS115200" -> Just CS115200
-    "110" -> Just CS110
-    "300" -> Just CS300
-    "600" -> Just CS600
-    "1200" -> Just CS1200
-    "2400" -> Just CS2400
-    "4800" -> Just CS4800
-    "9600" -> Just CS9600
-    "19200" -> Just CS19200
-    "38400" -> Just CS38400
-    "57600" -> Just CS57600
-    "115200" -> Just CS115200
-    otherwise -> Nothing
+          maybeReadCS s = case s of
+            "CS110" -> Just CS110
+            "CS300" -> Just CS300
+            "CS600" -> Just CS600
+            "CS1200" -> Just CS1200
+            "CS2400" -> Just CS2400
+            "CS4800" -> Just CS4800
+            "CS9600" -> Just CS9600
+            "CS19200" -> Just CS19200
+            "CS38400" -> Just CS38400
+            "CS57600" -> Just CS57600
+            "CS115200" -> Just CS115200
+            "110" -> Just CS110
+            "300" -> Just CS300
+            "600" -> Just CS600
+            "1200" -> Just CS1200
+            "2400" -> Just CS2400
+            "4800" -> Just CS4800
+            "9600" -> Just CS9600
+            "19200" -> Just CS19200
+            "38400" -> Just CS38400
+            "57600" -> Just CS57600
+            "115200" -> Just CS115200
+            _ -> Nothing
